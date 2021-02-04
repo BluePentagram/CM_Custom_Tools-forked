9634abd5196bb991cb8f6c5f99c8bb001177aa03
NathanHarbour <nathanharbour@gmail.com>
2021-02-04 17:10:42 -0600

    IngestionOutcomeDoer_ChangeHediffSeverity

---
+++ Source/CM_Custom_Tools/IngestionOutcomeDoers/IngestionOutcomeDoer_ChangeHediffSeverity.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using RimWorld;
+using Verse;
+
+namespace CM_Custom_Tools.IngestionOutcomeDoers
+{
+    public class IngestionOutcomeDoer_ChangeHediffSeverity : IngestionOutcomeDoer
+    {
+        public HediffDef hediffDef;
+
+        public float severity = -1f;
+
+        public bool isScalar = false;
+
+        public ChemicalDef toleranceChemical;
+
+        public bool divideByBodySize;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+        {
+            List<Hediff> hediffs = pawn.health.hediffSet.hediffs.Where(hd => hd.def == hediffDef).ToList();
+
+            foreach (Hediff hediff in hediffs)
+            {
+                float newSeverity = hediff.Severity;
+
+                float effect = severity;
+
+                if (divideByBodySize)
+                    effect /= pawn.BodySize;
+
+                AddictionUtility.ModifyChemicalEffectForToleranceAndBodySize(pawn, toleranceChemical, ref effect);
+
+                if (isScalar)
+                    newSeverity *= effect;
+                else
+                    newSeverity += effect;
+
+                hediff.Severity = newSeverity;
+            }
+        }
+    }
+}
