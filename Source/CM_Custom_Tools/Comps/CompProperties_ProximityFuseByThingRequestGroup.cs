b929fa5c12dc04b962ecd6f0d8ae78175f18db2a
NathanHarbour <nathanharbour@gmail.com>
2021-02-25 15:21:14 -0600

    Added CompProximityFuseByThingRequestGroup
    Sets of an explosive fuse based on the proximity of something from the specified ThingRequestGroup

---
+++ Source/CM_Custom_Tools/Comps/CompProperties_ProximityFuseByThingRequestGroup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace CM_Custom_Tools.Comps
+{
+    public class CompProperties_ProximityFuseByThingRequestGroup : CompProperties
+    {
+        public ThingRequestGroup group;
+
+        public float radius;
+
+        public CompProperties_ProximityFuseByThingRequestGroup()
+        {
+            compClass = typeof(CompProximityFuseByThingRequestGroup);
+        }
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            foreach (string item in base.ConfigErrors(parentDef))
+            {
+                yield return item;
+            }
+            if (parentDef.tickerType != TickerType.Normal)
+            {
+                yield return string.Concat("CompProximityFuseByThingRequestGroup needs tickerType ", TickerType.Rare, " or faster, has ", parentDef.tickerType);
+            }
+            if (parentDef.CompDefFor<CompExplosive>() == null)
+            {
+                yield return "CompProximityFuseByThingRequestGroup requires a CompExplosive";
+            }
+        }
+    }
+}
---
+++ Source/CM_Custom_Tools/Comps/CompProximityFuseByThingRequestGroup.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace CM_Custom_Tools.Comps
+{
+    [StaticConstructorOnStartup]
+    public class CompProximityFuseByThingRequestGroup : ThingComp
+    {
+        public CompProperties_ProximityFuseByThingRequestGroup Props => props as CompProperties_ProximityFuseByThingRequestGroup;
+
+        public override void CompTick()
+        {
+            if (Find.TickManager.TicksGame % 250 == 0)
+            {
+                CompTickRare();
+            }
+        }
+
+        public override void CompTickRare()
+        {
+            //if (GenClosest.ClosestThingReachable(parent.Position, parent.Map, ThingRequest.ForDef(Props.target), PathEndMode.OnCell, TraverseParms.For(TraverseMode.NoPassClosedDoors), Props.radius) != null)
+            if (GenClosest.ClosestThingReachable(parent.Position, parent.Map, ThingRequest.ForGroup(Props.group), PathEndMode.OnCell, TraverseParms.For(TraverseMode.NoPassClosedDoors), Props.radius) != null)
+            {
+                parent.GetComp<CompExplosive>().StartWick();
+            }
+        }
+    }
+}
